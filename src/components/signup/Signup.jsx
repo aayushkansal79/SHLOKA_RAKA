c7a4caec61ca9231ccd47d81c6da5f42120eb2fc
anish suyal <anishsuyal28@gmail.com>
2025-04-04 15:11:15 +0530

    try

--- src/components/signup/Signup.jsx
+++ src/components/signup/Signup.jsx
@@ -1,8 +1,36 @@
 import React from 'react'
+import './signup.css'
 
 const signup = () => {
   return (
-    <div>signup</div>
+    <div className='main-container'>
+      <div className='hearder-container' ></div>
+
+      <div className="signup-title">
+       <div className="red-box">
+          <p className='title'>
+            जानकारी दर्ज करे
+          </p>
+        </div>
+      </div> 
+
+      <div className="form-container">
+        <form className='form-content'>
+          <div className="name-container">
+            <label htmlFor="fullname">Full Name</label>
+            <label htmlFor="phonenumber">Phone Number</label>
+            <label htmlFor="city">City</label>
+            <label htmlFor="username">Username</label>
+          </div>
+          <div className="inpboxleft">
+            <input type="text" name="" id="fullname" />
+            <input type="text" name="" id="phonenumber" />
+            <input type="text" name="" id="city" />
+            <input type="text" name="" id="" />
+          </div>
+        </form>
+      </div>
+    </div>
   )
 }
 
